643087a9daf04e8ffc43fc0ac936905903603817
Akhand Vaibhav Singh <72245505+akhandvaibhav17@users.noreply.github.com>
2021-09-02 22:40:41 +0530

    Added images & projects.php

---
+++ projects.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Tara Mangal Foundation | Projects</title>
+    <!-- Stylesheets -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- Responsive File -->
+    <link href="css/responsive.css" rel="stylesheet">
+    <!-- Color File -->
+    <link href="css/color.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+    <!-- Responsive -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
+
+<body>
+
+    <div class="page-wrapper">
+        <!-- Preloader -->
+        <div class="preloader">
+            <div class="icon"></div>
+        </div>
+
+        <!-- Main Header -->
+        <?php   include "header.php"  ?>
+        <!-- End Main Header -->
+
+         <!-- Page Banner Section -->
+    <section class="page-banner">
+        <div class="image-layer lazy-image" data-bg="url('images/background/bg-banner-1.jpg')"></div>
+        <div class="bottom-rotten-curve"></div>
+
+        <div class="auto-container">
+            <h1>Our Projects</h1>
+            <ul class="bread-crumb clearfix">
+                <li><a href="index.html">Home</a></li>
+                <li class="active">Our Projects</li>
+            </ul>
+        </div>
+
+    </section>
+    <!--End Banner Section -->
+     
+
+    <section class="about-section style-three" style="background-image: url(C:\xampp\htdocs\TMF-Website - Copy\README.md\images\background\rotten-curve-1.png)">
+    
+        <div class="auto-container">
+          <div class="row clearfix">
+            <!--Left Column-->
+            <div class="left-column col-lg-5 col-md-12 col-sm-12">
+              <div class="inner">
+                <div class="sec-title">
+             
+                  <h2>Donation Drive</h2>
+                  <div class="text">
+                  Small contributions make a large impact and making donation is the ultimate sign of solidarity.
+                No matter how small it is, a donation will always carry its significance.
+                The purpose of launching this program named 'Donation Drive' is to provide facilities/essentials
+                and needful things to those who are unable to afford these due to some financial reasons, less awareness, lack of 
+                contancts or some or other reasons. The donations by Tara Mangal Foundation are sometimes made in form of
+                extremely necessary commodities like food and water, sometimes as needful things like blankets/woolens etc.
+                In some emergency cases our members also donate or arrange for 'blood/plasma' which has become quite frequent in covid period.
+                As of know Tara Mangal Foundation has done 3 'Needfuls' Donation' where about 50 people have been benefitted, many 
+'Essential's Donation' and 2 'Blood Donation Drives' by which 4 people have been benefitted and 1 life has been saved in emergency condition.
+                  </div>
+                  <div class="link-box clearfix">
+                    <a href="about.html" class="theme-btn btn-style-one"><span class="btn-title">View Gallery</span></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!--Right Column-->
+            <div class="right-column col-lg-7 col-md-12 col-sm-12">
+              <div class="inner">
+                <div class="images clearfix">
+                  <!--Images-->
+                  <div class="image-block">
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="0ms" style="visibility: visible; animation-delay: 0ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/donation-drive/1.jpg" data-src="images/resource/featured-image-9.jpg" alt="" data-was-processed="true">
+                    </figure>  
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="300ms" style="visibility: visible; animation-delay: 300ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/donation-drive/4.jpg" data-src="images/resource/featured-image-11.jpg" alt="" data-was-processed="true">
+                    </figure>
+                    
+                 
+                    
+                  </div>
+                  <div class="image-block">
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="300ms" style="visibility: visible; animation-delay: 300ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/donation-drive/2.jpeg" data-src="images/resource/featured-image-11.jpg" alt="" data-was-processed="true">
+                    </figure>
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="0ms" style="visibility: visible; animation-delay: 0ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/donation-drive/3.jpeg" data-src="images/resource/featured-image-9.jpg" alt="" data-was-processed="true">
+                    </figure>
+                  
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+       
+
+      
+
+    <section class="about-section ">
+    <div class="top-rotten-curve"></div>
+   
+        <div class="auto-container">
+          <div class="row clearfix">
+            <!--Left Column-->
+            <div class="left-column col-lg-5 col-md-12 col-sm-12">
+              <div class="inner">
+                <div class="sec-title">
+                
+                  <h2>Project Paryavaran</h2>
+                  <div class="text">
+                  The major rising topic all over the world is the 'Detiorating Environment' and there is a major need to take certain steps 
+                    towards its betterment. As the name suggests, 'Project Paryavaran' was launched to serve the mentioned purpose.
+                    Few programs have been organized under this project, where plantation drives have been carried out by the members 
+                    and the volunteers on the occassions of 'World Environment Day' and 'Van Mahotsav'. Over 30+ trees have been planted
+                    in the programs combined. We also promote new ideas that benefit the environment like organic farming, help for animals 
+                    upcycling of products etc.
+                  </div>
+                  <div class="link-box clearfix">
+                    <a href="about.html" class="theme-btn btn-style-one"><span class="btn-title">View Gallery</span></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!--Right Column-->
+            <div class="right-column col-lg-7 col-md-12 col-sm-12">
+              <div class="inner">
+                <div class="images clearfix">
+                  <!--Images-->
+                  
+                  <div class="image-block">
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="0ms" style="visibility: visible; animation-delay: 0ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/project-paryavaran/1.jpg" data-src="images/resource/featured-image-9.jpg" alt="" data-was-processed="true">
+                    </figure>
+                  </div>
+                  <div class="image-block">
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="900ms" style="visibility: visible; animation-delay: 900ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/project-paryavaran/3.jpeg" data-src="images/resource/featured-image-12.jpg" alt="" data-was-processed="true">
+                    </figure>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+       
+
+      <section class="about-section style-three">
+      
+      <div class="top-rotten-curve"></div> 
+        <div class="auto-container">
+          <div class="row clearfix">
+            <!--Left Column-->
+            <div class="left-column col-lg-5 col-md-12 col-sm-12">
+              <div class="inner">
+                <div class="sec-title">
+                  <h2>Health Is Wealth</h2>
+                  <div class="text">
+                  Medical facilites in metropoliants have been developed to a high extent, but in contrast to that the rural areas are still
+                  deprived of good quality health facilities. Many people suffer from chronic problems but lack of knowledge and facilities
+                  don't give chance to them for treatment. This problem gave Tara Mangal Foundation a chance to help those who required a solution.
+                  As of now Tara Mangal Foundation has organised 8 health camps in different rural areas of the eastern Uttar Pradesh region.
+                  These camps are of free of cost where patients are treated by specialized doctors for chest,ent,eye,neuro,cardio and many other commoon problems
+                  and are provided medicines prescribed for free. We assure to provide many more camps with our partners 'Tara Medicals,Mau' and
+                  'Smart Diagnostic Center' for the better health facilities in rural region.
+                  </div>
+                  <div class="link-box clearfix">
+                    <a href="about.html" class="theme-btn btn-style-one"><span class="btn-title">View Gallery</span></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!--Right Column-->
+            <div class="right-column col-lg-7 col-md-12 col-sm-12">
+              <div class="inner">
+                <div class="images clearfix">
+                  <!--Images-->
+                  <div class="image-block">
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="0ms" style="visibility: visible; animation-delay: 0ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/health-is-wealth/1.jpg" data-src="images/resource/featured-image-9.jpg" alt="" data-was-processed="true">
+                    </figure>
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="0ms" style="visibility: visible; animation-delay: 0ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/health-is-wealth/2.jpg" data-src="images/resource/featured-image-9.jpg" alt="" data-was-processed="true">
+                    </figure>
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="300ms" style="visibility: visible; animation-delay: 300ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/health-is-wealth/5.jpg" data-src="images/resource/featured-image-11.jpg" alt="" data-was-processed="true">
+                    </figure>
+                    
+                  </div>
+                  <div class="image-block">
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="300ms" style="visibility: visible; animation-delay: 300ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/health-is-wealth/3.jpg" data-src="images/resource/featured-image-11.jpg" alt="" data-was-processed="true">
+                    </figure>
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="0ms" style="visibility: visible; animation-delay: 0ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/health-is-wealth/4.jpg" data-src="images/resource/featured-image-9.jpg" alt="" data-was-processed="true">
+                    </figure>
+                    
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="0ms" style="visibility: visible; animation-delay: 0ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/health-is-wealth/6.jpg" data-src="images/resource/featured-image-9.jpg" alt="" data-was-processed="true">
+                    </figure>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+       
+
+      
+
+    <section class="about-section ">
+    <div class="top-rotten-curve"></div>
+    
+        <div class="auto-container">
+          <div class="row clearfix">
+            <!--Left Column-->
+            <div class="left-column col-lg-5 col-md-12 col-sm-12">
+              <div class="inner">
+                <div class="sec-title">
+                  <h2>Constructional Reformations</h2>
+                  <div class="text">
+                  Basic architectural facilities may not be neccessary but are important. In context to that TMF tries to do 
+                             constructional reformations that help in making life smooth.Some of our works under this project include
+                             repairing of manholes, setting up benches in public areas and installation of handpumps.   
+                  
+                  </div>
+                  <div class="link-box clearfix">
+                    <a href="about.html" class="theme-btn btn-style-one"><span class="btn-title">View Gallery</span></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!--Right Column-->
+            <div class="right-column col-lg-7 col-md-12 col-sm-12">
+              <div class="inner">
+                <div class="images clearfix">
+                  <!--Images-->
+                  <div class="image-block">
+                    
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="0ms" style="visibility: visible; animation-delay: 0ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/constructional-reformations/.jpeg" data-src="images/resource/featured-image-9.jpg" alt="" data-was-processed="true">
+                    </figure>
+               
+                  </div>
+                  <div class="image-block">
+                    
+                    <figure class="image-box wow fadeInUp animated" data-wow-delay="0ms" style="visibility: visible; animation-delay: 0ms; animation-name: fadeInUp;">
+                      <img class="lazy-image loaded" src="images/constructional-reformations/1.jpeg" data-src="images/resource/featured-image-9.jpg" alt="" data-was-processed="true">
+                    </figure>
+               
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+       
+     
+      
+  
+    <!--End pagewrapper-->
+
+    <!--Scroll to top-->
+    <div class="scroll-to-top scroll-to-target" data-target="html"><span class="flaticon-up-arrow"></span></div>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/jquery.fancybox.js"></script>
+    <script src="js/owl.js"></script>
+    <script src="js/appear.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/lazyload.js"></script>
+    <script src="js/scrollbar.js"></script>
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
